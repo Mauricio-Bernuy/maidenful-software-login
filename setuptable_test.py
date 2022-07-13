1dc1a8bce25585e9a5b55774c9f6a436b62a1a28
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-07-12 19:41:26 -0500

    a

--- setuptable_test.py
+++ setuptable_test.py
@@ -1,8 +1,8 @@
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 # import unittest
 import pytest
 from main import create_app
